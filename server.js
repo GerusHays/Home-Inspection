ae81ed7c11ed0e3105574697c291d69cc85acbb7
lrpineda <luicks212@gmail.com>
2022-06-15 20:04:14 -0600

    express stuff

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(404).send('404: Page not found');
+});
